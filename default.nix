9f9750be32b80c5d12e2ccf81663077904b6b68f
Bas van Dijk <v.dijk.bas@gmail.com>
2018-03-15 02:24:16 +0100

    WIP

---
+++ default.nix
@@ -0,0 +1,28 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, template-haskell, th-abstraction, containers, stdenv }:
+      mkDerivation {
+        pname = "stable-name-eq";
+        version = "0.0.0.0";
+        sha256 = "";
+        libraryHaskellDepends = [ base template-haskell th-abstraction containers ];
+        homepage = "https://github.com/basvandijk/stable-name-eq";
+        description = "Eq-like class that uses StableNames for fast equality checking";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
